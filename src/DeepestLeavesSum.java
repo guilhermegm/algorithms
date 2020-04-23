aa9a0e1726d7942bef2d8a909de2154fe573e634
Guilherme GM <321.guilherme@gmail.com>
2020-04-23 12:57:28 -0300

    add a better solution for deepest leaves sum

--- src/DeepestLeavesSum.java
+++ src/DeepestLeavesSum.java
@@ -1,10 +1,8 @@
-import com.sun.source.tree.Tree;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class DeepestLeavesSum {
-    public static int deepestLeavesSum(TreeNode root) {
+    public static int deepestLeavesSumBFS(TreeNode root) {
         if (root == null) return 0;
 
         Queue<TreeNode> queue = new LinkedList<>();
@@ -25,6 +23,26 @@ public class DeepestLeavesSum {
         return levelSum;
     }
 
+    // https://leetcode.com/problems/deepest-leaves-sum/discuss/592664/Java-0-ms-100-O(n)-single-tree-traversal-no-global-vars-explained
+    public static int deepestLeavesSumRecursive(TreeNode root) {
+        int[] levelsSum = new int[10000];
+        int i = calculateLevelSum(root, levelsSum, 0);
+        return levelsSum[i];
+    };
+
+    public static int calculateLevelSum(TreeNode node, int[] levelsSum, int i) {
+        if (node == null) return 0;
+
+        if (node.left == null && node.right == null) {
+            levelsSum[i] += node.val;
+            return i;
+        }
+
+        int left = calculateLevelSum(node.left, levelsSum, i + 1);
+        int right = calculateLevelSum(node.right, levelsSum, i + 1);
+        return Math.max(left, right);
+    }
+
     public static void main(String[] args) {
         TreeNode one = new TreeNode(1);
         TreeNode two = new TreeNode(2);
@@ -43,8 +61,7 @@ public class DeepestLeavesSum {
         four.left = seven;
         six.right = eight;
 
-        int r = deepestLeavesSum(one);
+        int r = deepestLeavesSumRecursive(one);
         System.out.println(r);
     }
 }
-
